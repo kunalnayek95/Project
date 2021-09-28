41c248b9992b54eb52c229989e881e2d94b75fbf
Kunal Nayek <kunal.nayek@esolzmail.com>
2021-09-28 12:52:30 +0530

    commit

---
+++ Swift/CollectionViewTest/CollectionViewTest/MyCVC.swift
@@ -0,0 +1,13 @@
+//
+//  MyCVC.swift
+//  CollectionViewTest
+//
+//  Created by Kunal's MacBook on 28/09/21.
+//
+
+import UIKit
+
+class MyCVC: UICollectionViewCell {
+    @IBOutlet weak var cellImg: UIImageView!
+    
+}
